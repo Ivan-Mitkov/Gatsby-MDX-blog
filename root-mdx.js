8398c776f27e013bcccd086e4fbf7d4e82882e21
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-07-19 15:36:14 +0300

    wrapRootElement with MDXProvider

---
+++ root-mdx.js
@@ -0,0 +1,10 @@
+import './src/css/main.css'
+import React from 'react'
+import { MDXProvider } from '@mdx-js/react'
+
+const components = {
+  //all the logic
+}
+export const wrapMDX = ({ element }) => {
+  return <MDXProvider components={components}>{element}</MDXProvider>
+}

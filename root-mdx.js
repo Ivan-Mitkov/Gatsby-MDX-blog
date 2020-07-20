31ea53a84819df4667b4d4d8ef6c5275cd59ebc0
Ivan Mitkov <ivan.mitkov@gmail.com>
2020-07-20 12:28:56 +0300

    more style to code elements

--- root-mdx.js
+++ root-mdx.js
@@ -1,7 +1,7 @@
 import './src/css/main.css'
 import React from 'react'
 import { MDXProvider } from '@mdx-js/react'
-import { Headings, Code, Blockquote, Example } from './src/components/Complete'
+import { Headings, Code, Blockquote, Example,PrismSetup } from './src/components/Complete'
 
 // const MyH2 = props => {
 //   if (props.title) {
@@ -28,7 +28,8 @@ const components = {
   h4: Headings.myH4,
   inlineCode: Code,
   blockquote: Blockquote,
-  pre: Example,
+  // pre: Example,
+  pre: PrismSetup,
 }
 export const wrapMDX = ({ element }) => {
   return <MDXProvider components={components}>{element}</MDXProvider>
